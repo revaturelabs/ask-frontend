9a075ddacda78944261fbaa7fdd9f78d753c053e
athavy92 <athavy92@gmail.com>
2019-10-22 16:03:00 -0400

    environment configurations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  environmentName: 'Default Environment',
+  localhostUri: 'http://localhost:8080/'
 };
 
 /*
