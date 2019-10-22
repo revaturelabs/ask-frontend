c65136a242d6f271088965d6089e88bea14b0e97
AdamShipe <54280663+AdamShipe@users.noreply.github.com>
2019-10-22 15:20:44 -0400

    cleared component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { QuestionListComponent } from './question-list/question-list.component';
 import { ViewQuestionComponent } from './view-question/view-question.component';
-import { QuestionComponent } from './question/question.component';
 import { QuestionPreviewComponent } from './question-preview/question-preview.component';
 import { ResponseComponent } from './response/response.component';
 import { AskQuestionComponent } from './ask-question/ask-question.component';
@@ -24,7 +23,6 @@ import { ExpertPageComponent } from './expert-page/expert-page.component';
     LoginPageComponent,
     QuestionListComponent,
     ViewQuestionComponent,
-    QuestionComponent,
     QuestionPreviewComponent,
     ResponseComponent,
     AskQuestionComponent,
