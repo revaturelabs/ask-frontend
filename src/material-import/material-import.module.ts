c8cc00e64fa148dfd748209f2af88d9415353605
AdamShipe <54280663+AdamShipe@users.noreply.github.com>
2019-10-23 13:54:20 -0400

    merged lots of components

--- src/material-import/material-import.module.ts
+++ src/material-import/material-import.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatMenuModule, MatButtonModule } from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatMenuModule, MatButtonModule],
-  exports: [MatMenuModule, MatButtonModule]
+  imports: [CommonModule, MatMenuModule, MatButtonModule, MatToolbarModule],
+  exports: [MatMenuModule, MatButtonModule, MatToolbarModule]
 })
 export class MaterialImportModule { }
