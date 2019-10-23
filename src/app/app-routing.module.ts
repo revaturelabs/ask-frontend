e323c97296a3b7876a549fee796c21e9e8d096c5
AdamShipe <54280663+AdamShipe@users.noreply.github.com>
2019-10-23 12:02:46 -0400

    navbars

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MatMenuModule, MatButtonModule } from '@angular/material';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), MatMenuModule, MatButtonModule],
+  exports: [RouterModule, MatMenuModule, MatButtonModule]
 })
 export class AppRoutingModule { }
