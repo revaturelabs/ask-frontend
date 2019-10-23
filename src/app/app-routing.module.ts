9f0f544cfb1e2f1bba7fddcce6673da19f0ddcf0
Nathan <alexcross17@gmail.com>
2019-10-23 10:49:23 -0400

    general login and routing to user & expert page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ExpertPageComponent } from './expert-page/expert-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component: LoginPageComponent
+  },
+  {
+    path: "expert-page",
+    component: ExpertPageComponent
+  },
+  {
+    path: "user-page",
+    component: UserPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
