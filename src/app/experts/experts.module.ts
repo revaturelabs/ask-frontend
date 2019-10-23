13f10d9a057241694636b0f60da00e681d406e58
AdamShipe <54280663+AdamShipe@users.noreply.github.com>
2019-10-23 14:43:03 -0400

    tweaks

--- src/app/experts/experts.module.ts
+++ src/app/experts/experts.module.ts
@@ -4,6 +4,7 @@ import { ExpertNavComponent } from './expert-nav/expert-nav.component';
 import { ExpertPageComponent } from './expert-page/expert-page.component';
 import { EnterResponseComponent } from './enter-response/enter-response.component';
 import { MaterialImportModule } from '../../material-import/material-import.module';
+import { SelfTagsComponent } from './self-tags/self-tags.component';
 
 
 
@@ -12,10 +13,12 @@ import { MaterialImportModule } from '../../material-import/material-import.modu
     ExpertNavComponent,
     ExpertPageComponent,
     EnterResponseComponent,
+    SelfTagsComponent,
   ],
   imports: [
     CommonModule,
     MaterialImportModule,
+    SelfTagsComponent,
   ]
 })
 export class ExpertsModule { }
