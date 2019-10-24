a5f792ace13272b0577b6a48040abd40f6573ff2
Kyungmin <kmlee0206@gmail.com>
2019-10-24 17:16:22 -0400

    ALL angular material form and CHIPS!

--- src/app/users/ask-question/ask-question.component.ts
+++ src/app/users/ask-question/ask-question.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {FormControl} from '@angular/forms';
+import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-ask-question',
@@ -7,7 +14,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AskQuestionComponent implements OnInit {
 
-  constructor() { }
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  tagCtrl = new FormControl();
+  filteredTags: Observable<string[]>;
+  tags: string[] = [];
+  allTags: string[] = ['Front-end', 'Back-end', 'Angular', 'HTML', 'CSS', 
+                        'JavaScript', 'Java', 'Spring','Database', 'SQL',
+                        'AWS', 'GCS', 'Azure'];
+
+  @ViewChild('tagInput', {static: false}) tagInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto', {static: false}) matAutocomplete: MatAutocomplete;
+
+  constructor() {
+    this.filteredTags = this.tagCtrl.valueChanges.pipe(
+        startWith(null),
+        map((tag: string | null) => tag ? this._filter(tag) : this.allTags.slice()));
+  }
+
+  add(event: MatChipInputEvent): void {
+    // Add fruit only when MatAutocomplete is not open
+    // To make sure this does not conflict with OptionSelected Event
+    if (!this.matAutocomplete.isOpen) {
+      const input = event.input;
+      const value = event.value;
+
+      // Add our fruit
+      if ((value || '').trim()) {
+        this.tags.push(value.trim());
+      }
+
+      // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+
+      this.tagCtrl.setValue(null);
+    }
+  }
+
+  remove(tag: string): void {
+    const index = this.tags.indexOf(tag);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.tags.push(event.option.viewValue);
+    this.tagInput.nativeElement.value = '';
+    this.tagCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allTags.filter(tag => tag.toLowerCase().indexOf(filterValue) === 0);
+  }
 
   ngOnInit() {
   }
--- src/material-import/material-import.module.ts
+++ src/material-import/material-import.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MatMenuModule, MatButtonModule, MatToolbarModule, MatInputModule, MatOptionModule, MatSelectModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+import { MatChipsModule } from '@angular/material/chips';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+// import {MatInputModule} from '@angular/material/input';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatMenuModule, MatButtonModule, MatToolbarModule, MatInputModule, MatOptionModule, MatSelectModule, FormsModule, ReactiveFormsModule],
-  exports: [MatMenuModule, MatButtonModule, MatToolbarModule, MatInputModule, MatOptionModule, MatSelectModule, FormsModule, ReactiveFormsModule]
+  imports: [CommonModule, MatMenuModule, MatButtonModule, MatToolbarModule, MatInputModule, MatOptionModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatChipsModule, MatAutocompleteModule, MatCheckboxModule, MatIconModule, MatButtonToggleModule],
+  exports: [MatMenuModule, MatButtonModule, MatToolbarModule, MatInputModule, MatOptionModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatChipsModule, MatAutocompleteModule, MatCheckboxModule, MatIconModule, MatButtonToggleModule]
 })
 export class MaterialImportModule { }
