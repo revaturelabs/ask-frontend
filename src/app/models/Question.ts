78201647f34c99bc827d9b4911b8365bd29f9dc3
athavy92 <athavy92@gmail.com>
2019-10-29 11:30:32 -0400

    added a few properties in Question.ts model
    Added a few properties that are needed in the existing Question.ts file in the models directory. Ensured added changes didn't break any functionality.

--- src/app/models/Question.ts
+++ src/app/models/Question.ts
@@ -1,5 +1,10 @@
 export interface Question {
   id: number;
+  username: string;
+  title: string;
+  description: string;
+  tags?: string;
+  date?: string;
   userId: number;
   head: string;
   body: string;
