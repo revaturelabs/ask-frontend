4c4a6d3fb136de2699201add2b1ddc2f54c28c48
Edward Sams <edwardsams@email.arizona.edu>
2019-10-29 21:00:30 -0400

    changes made to the expert response portion of front-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,11 @@ import { ResponseComponent } from './static/response/response.component';
 import { AskQuestionComponent } from './users/ask-question/ask-question.component';
 import { EnterResponseComponent } from './experts/enter-response/enter-response.component';
 import { SelfTagsComponent } from './experts/self-tags/self-tags.component';
-import { UserNavComponent } from './users/user-nav/user-nav.component';
-import { ExpertNavComponent } from './experts/expert-nav/expert-nav.component';
 import { MaterialImportModule } from '../material-import/material-import.module';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostsComponent } from './posts/posts.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './static/navbar/navbar.component';
 import { FooterComponent } from './static/footer/footer.component';
+import {ExpertPageComponent} from '../app/experts/expert-page/expert-page.component';
 
 @NgModule({
   declarations: [
@@ -35,13 +32,10 @@ import { FooterComponent } from './static/footer/footer.component';
     AskQuestionComponent,
     EnterResponseComponent,
     SelfTagsComponent,
-    UserNavComponent,
-    ExpertNavComponent,
     routingComponents,
-    PostFormComponent,
-    PostsComponent,
     NavbarComponent,
     FooterComponent,
+    ExpertPageComponent,
   ],
   imports: [
     BrowserModule,
