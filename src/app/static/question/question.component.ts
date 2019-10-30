bd2d3492169abd0707008e3390c7beec64f912da
AdamDKing <adamdentonking@gmail.com>
2019-10-30 11:38:30 -0400

    add input for question-related components

--- src/app/static/question/question.component.ts
+++ src/app/static/question/question.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { Question } from 'src/app/models/Question';
 
 @Component({
   selector: "app-question",
@@ -6,6 +7,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./question.component.css"]
 })
 export class QuestionComponent implements OnInit {
+  @Input() question: Question;
 
   constructor() {}
 
