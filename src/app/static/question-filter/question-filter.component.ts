fb44a8682554df1eb9e301ec7eaa902de3ea9be5
Borko Stankovic <borkostankovic83@yahoo.com>
2019-11-01 14:27:05 -0400

    search bar with chips

--- src/app/static/question-filter/question-filter.component.ts
+++ src/app/static/question-filter/question-filter.component.ts
@@ -1,29 +1,98 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {Component, ElementRef, ViewChild, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
+/**
+ * @title Chips Autocomplete
+ * @author Borko Stankovic
+ */
 @Component({
   selector: 'app-question-filter',
   templateUrl: './question-filter.component.html',
   styleUrls: ['./question-filter.component.css']
 })
 export class QuestionFilterComponent implements OnInit {
-  
-  question: string ="";
-  response: any;
-  constructor(private http: HttpClient) {
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  tagCtrl = new FormControl();
+  filteredTags: Observable<string[]>;
+  tags: string[] = ['Java'];
+  allTagsFromServer: string[] = ['Java', 'Angular'];
 
-   }
+  @ViewChild('tagInput', {static: false}) tagInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto', {static: false}) matAutocomplete: MatAutocomplete;
 
+  constructor() {
+    this.filteredTags = this.tagCtrl.valueChanges.pipe(
+        startWith(null),
+        map((tag: string | null) => tag ? this._filter(tag) : this.allTagsFromServer.slice()));
+    // this.ts.getTags().subscribe((tags) => {
+
+    //       for (let index = 0; index < tags.length; index++) {
+    //         this.allTagsFromServer.push(tags[index].tagName);
+    //       }
+
+    //       console.log(this.allTagsFromServer);
+    //   });
+
+      }
+
+  add(event: MatChipInputEvent): void {
+    // Add tag only when MatAutocomplete is not open
+    // To make sure this does not conflict with OptionSelected Event
+    if (!this.matAutocomplete.isOpen) {
+      const input = event.input;
+      const value = event.value;
+
+      // Add our tag
+      // Prevents inputing chips that is not on the list
+      if ((value || '').trim()) {
+        if (!this.allTagsFromServer.includes(value)) {
+          alert('Tag not recognized!')
+        } else {
+          this.tags.push(value.trim());
+        }
+      }
+
+      // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+
+      this.tagCtrl.setValue(null);
+    }
+  }
+
+  remove(tag: string): void {
+    const index = this.tags.indexOf(tag);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.tags.push(event.option.viewValue);
+    this.tagInput.nativeElement.value = '';
+    this.tagCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allTagsFromServer.filter(fruit => fruit.toLowerCase().indexOf(filterValue) === 0);
+  }
   ngOnInit() {
-    this.http.get('http://ec2-54-80-244-190.compute-1.amazonaws.com:1337/questions')
+    // this.form = this.fb.group({
+    // tags: ['']
+    // });
+    // this.ts.getTags();
   }
-
-  search() {
-    this.http.get('http://ec2-54-80-244-190.compute-1.amazonaws.com:1337/questions' + this.question)
-    .subscribe((response)=> {
-      this.response = response;
-      console.log(this.response);
-    })
-  }
-
 }
