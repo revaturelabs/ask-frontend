6cf188d6d1188f0f1beb53134ab867e4652b59a0
Zach Marshello <marsh3825@gmail.com>
2019-11-01 17:02:46 -0400

    update for tag-creation

--- src/app/experts/tag-creation/tag-creation.component.ts
+++ src/app/experts/tag-creation/tag-creation.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Tag } from '../../models/Tag';
 import { environment } from '../../../environments/environment';
 import {MatSnackBarModule, MatSnackBar} from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { SelfTagsComponent } from '../self-tags/self-tags.component';
 
 
 
@@ -13,18 +15,28 @@ import {MatSnackBarModule, MatSnackBar} from '@angular/material/snack-bar';
 })
 export class TagCreationComponent implements OnInit {
 
-  constructor(private http: HttpClient, private _snackBar: MatSnackBar) { }
+  constructor(private http: HttpClient, 
+              private _snackBar: MatSnackBar,   
+              private router: Router,
+              private selfTags: SelfTagsComponent,
+              ) { }
   tag: Tag = { id: 0, name: '' };
 
   ngOnInit() {
+
   }
 
 
   onSubmit() {
-    this._snackBar.open(`Tag Created`);
+    
     this.http.post(environment.tagsUri, this.tag)
     .subscribe(
       (result) => {
+        this._snackBar.open(`Tag Created`, 'OK', {duration: 4000});
+        this.selfTags.ngOnInit();
+        this.tag.name = "";
+      }, (failure) => {
+        this._snackBar.open(`Failed, possible duplicate tag.`, 'Try Again', {duration: 4000});
       }
     );
   }
