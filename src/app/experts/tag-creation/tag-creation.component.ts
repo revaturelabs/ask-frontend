ee033d2f2827113b7cf8ae8963257ec378096435
Zach Marshello <marsh3825@gmail.com>
2019-11-04 09:44:11 -0500

    Cleaned up code, fixed submit bug'

--- src/app/experts/tag-creation/tag-creation.component.ts
+++ src/app/experts/tag-creation/tag-creation.component.ts
@@ -2,19 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Tag } from '../../models/Tag';
 import { environment } from '../../../environments/environment';
-import {MatSnackBarModule, MatSnackBar} from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+import { MatSnackBar} from '@angular/material/snack-bar';
 import { SelfTagsComponent } from '../self-tags/self-tags.component';
-
-
 
 @Component({
   selector: 'app-tag-creation',
   templateUrl: './tag-creation.component.html',
   styleUrls: ['./tag-creation.component.css'],
 })
+
 export class TagCreationComponent implements OnInit {
-  constructor(private http: HttpClient, private _snackBar: MatSnackBar, private selfTags: SelfTagsComponent) {}
+  constructor(
+    private http: HttpClient, 
+    private snackBar: MatSnackBar, 
+    private selfTags: SelfTagsComponent
+    ) {}
+  
   tag: Tag = { id: 0, name: '' };
 
   ngOnInit() {}
@@ -24,10 +27,10 @@ export class TagCreationComponent implements OnInit {
       result => {
         this.selfTags.ngOnInit();
         this.tag.name = "";
-        this._snackBar.open(`Tag Created`, `OK`, { duration: 2000 });
+        this.snackBar.open(`Tag Created`, `OK`, { duration: 2000 });
       },
       error => {
-        this._snackBar.open(`Failed to Create Tag`, `OK`, { duration: 2000 });
+        this.snackBar.open(`Failed to Create Tag`, `OK`, { duration: 2000 });
       },
     );
   }
