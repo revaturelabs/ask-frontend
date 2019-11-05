a99b7785885e299201d9def7b9c4b705aba45548
JGworek <jonathangworek@gmail.com>
2019-11-05 16:19:11 -0500

    routing for user functionality

--- src/app/experts/enter-response/enter-response.component.ts
+++ src/app/experts/enter-response/enter-response.component.ts
@@ -4,15 +4,14 @@ import { ResponseService } from '../../services/response.service';
 import { QuestionService } from '../../services/question.service';
 import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import {MatSnackBar} from '@angular/material/snack-bar';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-enter-response',
   templateUrl: './enter-response.component.html',
   styleUrls: ['./enter-response.component.css'],
 })
-
 export class EnterResponseComponent implements OnInit {
   @Output() newResponse: EventEmitter<Response> = new EventEmitter();
   @Output() updatedResponse: EventEmitter<Response> = new EventEmitter();
@@ -26,12 +25,15 @@ export class EnterResponseComponent implements OnInit {
     creationDate: null,
   };
 
-  constructor(private responseService: ResponseService,
-              private questionService: QuestionService,
-              private authService: AuthService,
-              private _snackBar: MatSnackBar) {}
-    
-    questionId : number;
+  constructor(
+    private responseService: ResponseService,
+    private questionService: QuestionService,
+    private authService: AuthService,
+    private _snackBar: MatSnackBar,
+    private router: Router,
+  ) {}
+
+  questionId: number;
 
   ngOnInit() {
     this.questionId = this.questionService.getQuestionId();
@@ -39,26 +41,32 @@ export class EnterResponseComponent implements OnInit {
 
   addResponse(body) {
     if (!body) {
-      this._snackBar.open("Please add a Response!", "OK!", {duration: 3000});
+      this._snackBar.open('Please add a Response!', 'OK!', { duration: 3000 });
     } else {
       this.response.body = body;
       this.response.questionId = this.questionId;
       this.response.responderId = this.authService.account.id;
-      this.responseService
-        .saveResponse(this.response)
-        .subscribe(response => {
+      this.responseService.saveResponse(this.response).subscribe(
+        response => {
           this.newResponse.emit(response);
-          this._snackBar.open("Thank you for your Response", " ", {duration: 3000});
-        });
+          this._snackBar.open('Thank you for your Response', ' ', {
+            duration: 3000,
+          });
+          this.router.navigate(['/expert-questions']);
+        },
+        failure => {
+          this._snackBar.open('Response Submission Failed', ' ', {
+            duration: 3000,
+          });
+        },
+      );
     }
   }
 
   updateResponse() {
-    this.responseService
-      .updateResponse(this.response)
-      .subscribe(response => {
-        this.isEdit = false;
-        this.updatedResponse.emit(response);
-      });
+    this.responseService.updateResponse(this.response).subscribe(response => {
+      this.isEdit = false;
+      this.updatedResponse.emit(response);
+    });
   }
 }
