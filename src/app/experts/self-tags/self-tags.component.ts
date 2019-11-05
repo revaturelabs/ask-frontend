7c2d99d7bc616505f9b539bf82554a70c61e313f
Nick Brinson <nickbrinson3@gmail.com>
2019-11-05 14:16:56 -0500

    Implement update of expert associated tags and complete documentation

--- src/app/experts/self-tags/self-tags.component.ts
+++ src/app/experts/self-tags/self-tags.component.ts
@@ -3,11 +3,19 @@ import { Tag } from '../../models/Tag';
 import { TagService } from 'src/app/services/tags.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
+
+/**
+ * @author Zach Marshello, Adam Shipe, Nick Brinson
+ * 
+ * Populate list of available tags to associate with expert. Expert checks and unchecks tag names based on his or her skillset.
+ * Expert clicks submit to update associated tags in database.
+ * 
+ */
+
 @Injectable({
   providedIn: 'root'
 })
 
-//God help you
 @Component({
   selector: 'app-self-tags',
   templateUrl: './self-tags.component.html',
@@ -17,29 +25,17 @@ export class SelfTagsComponent implements OnInit {
   tags: Tag[];
   isChecked: boolean = false;
 
-  userId = this.authService.account.id;
-  // expertStartingTags: Tag[];
-  expertStartingTags: string[];
+  expertId = this.authService.account.id;
   expertSkills: string[] = [];
   currentExpert: any;
 
   constructor(private tagService: TagService, private authService: AuthService) { }
 
-  // eval = this.evaluation();
-  // evaluation() {
-  //   if (this.expertStartingTags.includes('Java')) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
-
+  // Check and uncheck tags use toggle to add and remove tags from list of expert associated tags to be updated
   toggle($event, id) {
     if ($event.checked === true) {
-      console.log(id + ' is checked!');
       this.expertSkills.push(id);
     } else {
-      console.log(id + ' is unchecked!');
       //delete the value and close the empty array slot
       this.expertSkills.splice(this.expertSkills.indexOf(id), 1);
     }
@@ -47,12 +43,12 @@ export class SelfTagsComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log("blah");
     this.tagService.saveExpertTags(this.expertSkills.map<Tag>(t=>{return {"name":t, "id":0}}), this.authService.account.id).subscribe(
       console.log, console.error
     );
   }
 
+  // For initialization, currentExpert associated tags are prechecked in list of tags
   checkExpertTags() {
     for (let i = 0; i < this.tags.length; i++) {
       this.currentExpert.expertTags.forEach(element => {
@@ -69,17 +65,10 @@ export class SelfTagsComponent implements OnInit {
   ngOnInit() {
     this.tagService.getTags().subscribe(tags => {
       this.tags = tags;
-      console.log(tags);
     });
 
-    this.tagService.getExpertTags(this.userId).subscribe((expert) => {
+    this.tagService.getExpertTags(this.expertId).subscribe((expert) => {
       this.currentExpert = expert;
-      //console.log(this.currentExpert.expertTags[0].name);
-      console.log(this.authService.account);
-      // console.log(expert.expertTags);
-      // var a = 'hello from ts';
-      // this.skillset = this.tags;
-      // document.getElementById('abc').innerHTML=a;
       this.checkExpertTags();
     });
     
