99697f5ee9d1578c0fbfac343f06b1ecc8842b38
athavy92 <athavy92@gmail.com>
2019-11-05 13:49:35 -0500

    added displaying images functionality

--- src/app/static/question/question.component.ts
+++ src/app/static/question/question.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Question } from 'src/app/models/Question';
+import { HttpClient } from '@angular/common/http';
+import { QuestionService } from '../../services/question.service';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-question',
@@ -9,7 +13,28 @@ import { Question } from 'src/app/models/Question';
 export class QuestionComponent implements OnInit {
   @Input() question: Question;
 
-  constructor() {}
+  constructor(
+    private httpClient: HttpClient,
+    private questionService: QuestionService,
+    private domSanitizer: DomSanitizer,
+    ) {}
 
-  ngOnInit() {}
+  image: any;
+  questionImagesUri: string = ' http://ec2-54-80-244-190.compute-1.amazonaws.com:1337/questions';
+
+  getPhotos() {
+    let observable = this.httpClient.get(`${this.questionImagesUri}/${this.questionService.getQuestionId()}/images`);
+    observable.subscribe((result: any) => {
+      let imageURL = 'data:image/jpeg;base64,' + result[0].image;
+      this.image = this.domSanitizer.bypassSecurityTrustUrl(imageURL);
+    },
+      (err) => {
+        console.log('Invalid');
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.getPhotos();
+  }
 }
