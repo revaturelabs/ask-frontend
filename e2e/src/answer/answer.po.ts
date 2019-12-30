e6a3e83dcde6747216d05df1d4db2216e88825df
Dan Kim <dan.dkyu.kim@gmail.com>
2019-12-30 11:30:42 -0500

    added protractor test for not leaving the page after highlighting

--- e2e/src/answer/answer.po.ts
+++ e2e/src/answer/answer.po.ts
@@ -7,7 +7,20 @@ export class AnswerPage extends AppPage {
     this.login(3);
     element(by.css('[routerlink="/expert-questions"]')).click();
   }
+  navigateToHighlightResponse() {
+    this.navigateTo();
+    this.login(1);
+    element(by.css('[routerlink="/user-questions"]')).click();
+  }
   selectQuestion(question: number) {
     element(by.id('q=' + question)).click();
+  }
+  highlightResponse(response: number) {
+    browser
+      .actions()
+      .mouseMove(element(by.id('r=' + response)))
+      .perform();
+    browser.sleep(1000);
+    element(by.id('btn7')).click();
   }
 }
