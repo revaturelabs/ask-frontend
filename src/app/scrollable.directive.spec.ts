9349aff93340a837928327b27832dd7a930c3a1c
notexecutive <tshirtyman@gmail.com>
2019-12-30 09:06:19 -0500

    changes

---
+++ src/app/scrollable.directive.spec.ts
@@ -0,0 +1,9 @@
+import { ScrollableDirective } from './scrollable.directive';
+import { ElementRef } from '@angular/core';
+
+describe('ScrollableDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ScrollableDirective();
+    expect(directive).toBeTruthy();
+  });
+});
