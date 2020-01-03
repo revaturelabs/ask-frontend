48ac77e2c43964bc7964ba306a18be30ba3e2b8d
lilac180 <purple.flowers.180@gmail.com>
2020-01-03 14:47:26 -0500

    fixed

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -10,7 +10,7 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './src/**/*.e2e-spec.ts'
+    './src/**/highlight.e2e-spec.ts'
   ],
   capabilities: {
     'browserName': 'chrome'
