ec62d235cce3cb14d65b0d1473e12ff0bff5499f
Joe Domingo <contact@joedomingo.online>
2020-02-12 12:36:36 -0500

    profile template started

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,11 +4,10 @@ import { AskQuestionComponent } from './ask-question/ask-question.component';
 import { MaterialImportModule } from '../../material-import/material-import.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AssociateProfileComponent } from './associateprofile/associateprofile.component';
-import { ExpertprofileComponent } from './expertprofile/expertprofile.component';
 import { ExpertProfileComponent } from './expert-profile/expert-profile.component';
 
 @NgModule({
-  declarations: [AskQuestionComponent, AssociateProfileComponent, ExpertprofileComponent, ExpertProfileComponent],
+  declarations: [AskQuestionComponent, AssociateProfileComponent, ExpertProfileComponent],
   imports: [
     CommonModule,
     MaterialImportModule,
