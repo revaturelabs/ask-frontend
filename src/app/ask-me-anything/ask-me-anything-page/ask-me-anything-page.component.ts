31e7e7eb15a45f18780db08f4ac731cd5bcce240
Alex Mao <alexmao@Alexs-MacBook-Pro.local>
2020-02-13 16:57:11 -0500

    I can chat with myself

--- src/app/ask-me-anything/ask-me-anything-page/ask-me-anything-page.component.ts
+++ src/app/ask-me-anything/ask-me-anything-page/ask-me-anything-page.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, ViewChild, ElementRef, Input, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-ask-me-anything-page',
   templateUrl: './ask-me-anything-page.component.html',
   styleUrls: ['./ask-me-anything-page.component.css']
 })
-export class AskMeAnythingPageComponent implements OnInit {
+export class AskMeAnythingPageComponent implements OnInit, AfterViewInit {
+  ngAfterViewInit(): void {
+    this.scrollToBottom();
+  }
+
+  @ViewChild('messageBox',{static : false}) messageBox : ElementRef;
+  @ViewChild('inputBox', {static : false}) inputBox : ElementRef;
+
+  messages : string[] = [
+  ]
+
+  @Input()
+  newMessage : string = '';
+
 
   constructor() { }
 
   ngOnInit() {
+
   }
+
+  @Output()
+  action(text : string){
+
+    if(text){
+      this.messages.push(text);
+      this.scrollToBottom();
+    }
+    
+  }
+
+  scrollToBottom(): void {
+    try {
+        this.messageBox.nativeElement.scrollTop = this.messageBox.nativeElement.scrollHeight;
+    } catch(err) { }                 
+}
 
 }
