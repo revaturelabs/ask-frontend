94c2670ce155a4b13636dc759a5b332396e2e2a1
Alex Mao <alexmao@Alexs-MacBook-Pro.local>
2020-02-18 10:12:30 -0500

    merging

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './static/login-page/login-page.component';
 import { AskQuestionComponent } from './users/ask-question/ask-question.component';
-import { ViewQuestionComponent } from './static/view-question/view-question.component';
+import { ViewQuestionComponent } from './question/view-question/view-question.component';
 import { QuestionListComponent } from './static/question-list/question-list.component';
 import { AuthGuard } from './services/auth/auth.guard';
 import { QuestionFilterComponent } from './static/question-filter/question-filter.component';
