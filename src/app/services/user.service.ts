6b9ed88b04f32ac03dfa8bc6a46090f8d9e35670
Jesse Yuille <Jesseyuille@yahoo.com>
2020-02-18 15:58:45 -0500

    default image, upload, changed link, added reactive forms

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -38,10 +38,7 @@ export class UserService {
   }
 
   updateProfilePic(profilePic: FormData, id: number){
-    this.http.patch(`${environment.userProfile}/${id}`, profilePic)
-    .subscribe((imageLink: string)=> {
-      return imageLink;
-    })
+    return this.http.patch<string>(`${environment.userProfile}/${id}`, profilePic);
   }
 
   createUser(user:User){
--- src/app/users/profile/profile.component.ts
+++ src/app/users/profile/profile.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { Markdownoptions } from 'src/app/models/markdownoptions';
 import { Tag } from '../../models/Tag';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-profile',
@@ -10,22 +13,54 @@ import { Tag } from '../../models/Tag';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private authService: AuthService) { 
+  constructor(private authService: AuthService, private fb: FormBuilder, private userService: UserService) {
     this.options.hideIcons = ['FullScreen'];
     this.options.showPreviewPanel = false;
     this.inEditMode = false;
     this.editModeText = "Personalize";
+    this.defaultPic = "../../assets/images/defaultProfilePic.png";
   }
 
   options: Markdownoptions = new Markdownoptions();
-  user = this.authService.user;
-  isAssociate = () : boolean => false;
-  selectedFile: File;
+  isAssociate = (): boolean => false;
+  selectedFile: File = null;
   inEditMode: boolean;
   editModeText: string;
+  profileForm: FormGroup;
+  defaultPic: string;
 
   ngOnInit() {
-    console.log(this.authService.user.isExpert);
+    this.profileForm = this.fb.group({
+      username: [''],
+      email: [''],
+      bio: [''],
+    });
+    
+    if (this.user.profilePic === null) {
+      this.user.profilePic = this.defaultPic;
+    }
+  }
+
+  updateProfile() {
+    if (this.profileForm.dirty) {
+      if (this.profileForm.value['username'] !== null) {
+        this.user.username = this.profileForm.value['username'];
+      }
+
+      if (this.profileForm.value['email'] !== null) {
+        this.user.email = this.profileForm.value['email'];
+      }
+
+      if (this.profileForm.value['bio'] !== null) {
+        this.user.bio = this.profileForm.value['bio'];
+      }
+      this.userService.updateUser(this.user,this.user.id);
+      this.profileForm.reset();
+    }
+
+    if (this.selectedFile !== null) {
+      this.onUpload();
+    }
   }
 
   onFileChange(event) {
@@ -34,14 +69,17 @@ export class ProfileComponent implements OnInit {
 
   onUpload() {
     const uploadData = new FormData();
-    uploadData.append('myImage', this.selectedFile, this.selectedFile.name)
+    uploadData.append('image', this.selectedFile, this.selectedFile.name)
 
-    this.authService.userProfilePic(uploadData);
+    this.userService.updateProfilePic(uploadData,this.user.id).subscribe(imageLink => {
+      this.user.profilePic = imageLink;
+    });
   }
 
   editMode() {
-   this.inEditMode = !this.inEditMode;
-   this.editModeText = (this.inEditMode) ? "Normal View" : "Personalize";
+    this.profileForm.reset();
+    this.inEditMode = !this.inEditMode;
+    this.editModeText = (this.inEditMode) ? "Normal View" : "Personalize";
   }
 
 }
