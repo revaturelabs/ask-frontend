125750b1129c8739c5956f868c4acf35294b0d1f
AdamDKing <adamdentonking@gmail.com>
2020-02-18 10:05:36 -0500

    remove duplicate import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MarkdownModule } from 'ngx-markdown';
