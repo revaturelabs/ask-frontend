76b47a8f9c322168247aa3aad0334a4a1be9055c
Alex Mao <alexmao@Alexs-MacBook-Pro.local>
2020-02-19 10:21:38 -0500

    fixed the list

--- src/app/experts/response-list/response-list.component.ts
+++ src/app/experts/response-list/response-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Question } from 'src/app/models/Question';
 import { QuestionService } from 'src/app/services/question.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-response-list',
@@ -11,10 +12,10 @@ export class ResponseListComponent implements OnInit {
 
   questionList : Question[];
 
-  constructor(private questionService : QuestionService) { }
+  constructor(private questionService : QuestionService, private authService : AuthService) { }
 
   ngOnInit() {
-    //hardcoded the 1 for now because I'm told there's something wacky with the questions system right now.
+    //hardcoded the  for now because I'm told there's something wacky with the questions system right now.
     this.questionService.getExpertQuestionsByResponse(3).subscribe((data)=>{
       console.log(data);
       this.questionList = data;
