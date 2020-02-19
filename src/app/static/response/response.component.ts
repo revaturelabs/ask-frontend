b3d5d56e15291f993ee4395fde5554d87644f3d2
AdamDKing <adamdentonking@gmail.com>
2020-02-19 09:46:14 -0500

    remove unused functionality and HTTP calls in requests

--- src/app/static/response/response.component.ts
+++ src/app/static/response/response.component.ts
@@ -16,10 +16,9 @@ import { Tag } from '../../models/Tag';
 })
 export class ResponseComponent implements OnInit, AfterViewChecked {
   @Input() response: Response;
+  @Input() highlightable: boolean;
   @Output() highlighted = new EventEmitter<boolean>();
 
-  responses: Response[];
-  isEdit = false;
   responderName: string;
   expertTags = [];
   hoverToggle = false;
@@ -30,16 +29,10 @@ export class ResponseComponent implements OnInit, AfterViewChecked {
   // represents how many skills from the skillset can fit across the response
   limit = this.expertTags.length;
 
-  // Only the user who asked the question can highlight a response
-  currentQuestionerId: number;
-  currentUserId: any;
-  currentQuestionObject: any;
   env = environment.questionsUri;
 
   constructor(
     private http: HttpClient,
-    private questionService: QuestionService,
-    private authService: AuthService,
     private _snackBar: MatSnackBar,
     private responseService: ResponseService,
     private cdRef: ChangeDetectorRef,
@@ -67,45 +60,6 @@ export class ResponseComponent implements OnInit, AfterViewChecked {
     this.resizedPage();
   }
 
-  onNewResponse(response: Response) {
-    this.responses.unshift(response);
-  }
-
-  editResponse(response: Response) {
-    this.response = response;
-    this.isEdit = true;
-  }
-
-  onUpdatedResponse(response: Response) {
-    this.responses.forEach((cur, index) => {
-      if (response.id === cur.id) {
-        this.responses.splice(index, 1);
-        this.responses.unshift(response);
-        this.isEdit = false;
-        this.response = {
-          user: null,
-          id: 0,
-          responderId: 0,
-          questionId: 0,
-          body: '',
-          creationDate: '',
-        };
-      }
-    });
-  }
-
-  removeResponse(response: Response) {
-    if (confirm('Are You Sure?')) {
-      this.responseService.removeResponse(response.id).subscribe(() => {
-        this.responses.forEach((cur, index) => {
-          if (response.id === cur.id) {
-            this.responses.splice(index, 1);
-          }
-        });
-      });
-    }
-  }
-
   showTagsList(startIndex: number) {
     const tagsList = [];
     for (let i = startIndex; i < this.expertTags.length; i++) {
@@ -115,24 +69,10 @@ export class ResponseComponent implements OnInit, AfterViewChecked {
   }
 
   ngOnInit() {
-    this.responseService.getResponses().subscribe(responses => {
-      this.responses = responses;
-    });
-
     this.responseService.getResponseById(this.response.id).subscribe(result => {
       this.responderName = result.user.username;
       this.expertTags = result.user.expertTags;
     });
-
-    const observable = this.http.get(
-      `${environment.questionsUri}/${this.questionService.getQuestionId()}`,
-    );
-    observable.subscribe(result => {
-      this.currentQuestionObject = result;
-      this.currentQuestionerId = this.currentQuestionObject.questionerId;
-      this.currentUserId = this.authService.account.id;
-    });
-
     this.hiddenExpertTags = [];
   }
 
