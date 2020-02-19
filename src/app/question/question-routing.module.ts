4d10deb689bc781c2dbf0e695a77498d14c1c46d
herringtonca <herringtonca@mymail.vcu.edu>
2020-02-19 14:29:31 -0500

    refactored 'all questions' and 'assigned questions' as a filter option

--- src/app/question/question-routing.module.ts
+++ src/app/question/question-routing.module.ts
@@ -3,13 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { ViewQuestionComponent } from '../question/view-question/view-question.component';
 import { AuthGuard } from '../services/auth/auth.guard';
 import { FilteredQuestionListComponent } from '../question/filtered-question-list/filtered-question-list.component';
-import { ExpertQuestionsComponent } from '../question/expert-questions/expert-questions.component';
 
 const routes: Routes = [{
     path: 'questions', children: [
         { path: 'view', component: ViewQuestionComponent, canActivate: [AuthGuard] },
         { path: '', component: FilteredQuestionListComponent, canActivate: [AuthGuard] },
-        { path: 'expert', component: ExpertQuestionsComponent, canActivate: [AuthGuard] },
     ]
 }];
 
--- src/app/question/question.module.ts
+++ src/app/question/question.module.ts
@@ -6,7 +6,6 @@ import { ViewQuestionComponent } from '../question/view-question/view-question.c
 import { QuestionComponent } from '../question/question/question.component';
 import { QuestionFilterComponent } from '../question/question-filter/question-filter.component';
 import { FilteredQuestionListComponent } from '../question/filtered-question-list/filtered-question-list.component';
-import { ExpertQuestionsComponent } from '../question/expert-questions/expert-questions.component';
 import { EnterResponseComponent } from './enter-response/enter-response.component';
 import { HighlightedResponseComponent } from './highlighted-response/highlighted-response.component';
 import { ResponseComponent } from './response/response.component';
@@ -23,7 +22,6 @@ import { QuestionRoutingModule } from "./question-routing.module";
     ViewQuestionComponent,
     QuestionFilterComponent,
     FilteredQuestionListComponent,
-    ExpertQuestionsComponent,
     EnterResponseComponent,
     HighlightedResponseComponent,
     ResponseComponent
@@ -40,7 +38,6 @@ import { QuestionRoutingModule } from "./question-routing.module";
     ViewQuestionComponent,
     QuestionFilterComponent,
     FilteredQuestionListComponent,
-    ExpertQuestionsComponent,
     EnterResponseComponent,
     HighlightedResponseComponent,
     ResponseComponent
