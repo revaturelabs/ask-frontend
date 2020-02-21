91d4a0de96cd3e724547b761e8d9d5c6afccc9c1
Alexander Mao <alexmao@gmail.com>
2020-02-21 16:57:24 -0500

    help

--- src/app/ask-me-anything/ama-event/ama-event.component.ts
+++ src/app/ask-me-anything/ama-event/ama-event.component.ts
@@ -21,10 +21,27 @@ export class AmaEventComponent implements OnInit {
   }
 
   parseDate(){
-    return `${this.event.date.getMonth()} ${this.event.date.getDate()}`;
+    return `${this.getMonthString(this.event.date.getMonth())} ${this.event.date.getDate()}`;
   }
 
   parseTime() : string{
-    return  `${this.event.date.getHours()%12}:${this.event.date.getMinutes()} ${this.event.date.getHours()%12 ? 'AM':'PM'}`;
+    return  `${this.event.date.getHours()%12}:${this.event.date.getMinutes()} ${this.event.date.getHours()%12 ? 'PM':'AM'}`;
+  }
+
+  getMonthString(monthNum : number) : string{
+    switch(monthNum){
+      case 1 : return 'JAN';
+      case 2 : return 'FEB';
+      case 3 : return 'MAR';
+      case 4 : return 'APR';
+      case 5 : return 'MAY';
+      case 6 : return 'JUN';
+      case 7 : return 'JUL';
+      case 8 : return 'AUG';
+      case 9 : return 'SEP';
+      case 10: return 'OCT';
+      case 11: return 'NOV';
+      case 12: return 'DEC';
+    }
   }
 }
--- src/app/services/ama.service.ts
+++ src/app/services/ama.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { AmaSession } from '../models/ama-session';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AmaService {
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) { }
+
+  getAmaListByExpert(expertName : string) : Observable<AmaSession[]>{
+    return this.httpClient.get<AmaSession[]>(`http://localhost:1337/amaSession/${expertName}`);
+  }
+
+  getAmaList(){
+    return this.httpClient.get<AmaSession[]>(`http://localhost:1337/amaSession`);
+  }
 }
