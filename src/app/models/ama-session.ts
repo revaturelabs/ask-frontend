427b99d0815a268647dd2eaff7fa1693cd95f463
Alexander Mao <alexmao@gmail.com>
2020-02-21 09:46:29 -0500

    added class fields

--- src/app/models/ama-session.ts
+++ src/app/models/ama-session.ts
@@ -1,2 +1,13 @@
 export class AmaSession {
+    id : number;
+    date : Date;
+    topic : string;
+    expert : string;
+
+    Constructor(id : number, date : Date, topic : string, expert : string){
+        this.id = id;
+        this.date = date;
+        this.topic = topic;
+        this.expert = expert;
+    }
 }
