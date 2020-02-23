3ffd197e8043002fe8c904d6d1e41deaeb3d2bdc
EdmondDillonPhillips <edmond.dillon.phillips@gmail.com>
2020-02-23 11:01:15 -0500

    AMASession service methods don't hit backend routes

--- src/app/services/ama.service.ts
+++ src/app/services/ama.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AmaSession } from '../models/ama-session';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AmaService {
 
-  constructor() { }
+
+  constructor(public http: HttpClient) { }
+
+  
+  getSession(): Observable<AmaSession> {
+    console.log(environment.amaSessionUri)
+    return this.http.get<AmaSession>(environment.amaSessionUri);
+  }
+  
+
+  createSession(createAmaSession: AmaSession): Observable<AmaSession> {
+    console.log("The date created is: " + createAmaSession.date)
+    console.log("The object createAmaSession is: " + createAmaSession);
+    console.log("The URI is: " + environment.amaSessionUri)
+    return this.http.post<AmaSession>(
+      environment.amaSessionUri,
+      createAmaSession,
+      httpOptions
+  );
+  }
+
+
 }
--- src/app/static/schedule-amasession/schedule-amasession/schedule-amasession.component.ts
+++ src/app/static/schedule-amasession/schedule-amasession/schedule-amasession.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AmaSession } from '../../../models/ama-session'
+import { AmaService } from 'src/app/services/ama.service';
 
 @Component({
   selector: 'app-schedule-amasession',
@@ -7,13 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleAmasessionComponent implements OnInit {
 
-  constructor() { }
+
+  myDate = new Date();
+  session: AmaSession = new AmaSession(7,this.myDate,'blah','John Revature');
+
+
+  constructor(public amaService: AmaService) { }
 
   ngOnInit() {
   }
 
-  testClick(){
-    console.log("You clicked the thing")
+  postClick(){
+    this.session.date = this.myDate;
+    console.log("createSession()");
+    console.log("Your id is: " + this.session.id);
+    console.log("Your date is: " + this.session.date);
+    console.log("Your topic is: " + this.session.topic);
+    console.log("Your expert is: " + this.session.expert);
+    this.amaService.createSession(this.session);
+  }
+  getClick(){
+    this.session.date = this.myDate;
+    console.log("getSession()");
+    console.log("Your id is: " + this.session.id);
+    console.log("Your date is: " + this.session.date);
+    console.log("Your topic is: " + this.session.topic);
+    console.log("Your expert is: " + this.session.expert);
+    this.amaService.getSession();
   }
 
 }
