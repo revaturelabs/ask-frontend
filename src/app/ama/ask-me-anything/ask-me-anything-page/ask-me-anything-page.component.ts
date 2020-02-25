a44f0e4caeba2c2f9d9c27b5f805a1b9b7d05b2f
Garrett Jones <mgjones130@gmail.com>
2020-02-25 14:35:56 -0500

    Modals

--- src/app/ama/ask-me-anything/ask-me-anything-page/ask-me-anything-page.component.ts
+++ src/app/ama/ask-me-anything/ask-me-anything-page/ask-me-anything-page.component.ts
@@ -117,7 +117,9 @@ export class AskMeAnythingPageComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewChecked(){
-    this.messageBox.scrollToBottom();
+    if(this.messageBox) {
+      this.messageBox.scrollToBottom();
+    }
   }
 
 }
